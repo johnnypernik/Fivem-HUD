4d566c60a4dbde3238f174951f5ced351ba3060e
Samnick86 <jurikj86@gmail.com>
2019-10-27 12:30:57 +0100

    Create cli.lua

---
+++ cli.lua
@@ -0,0 +1,111 @@
+ESX = nil
+local isTalking = false
+--In Vehicle Speak = false
+
+Citizen.CreateThread(function()
+	while ESX == nil do
+		TriggerEvent('esx:getSharedObject', function(obj) ESX = obj end)
+		Citizen.Wait(0)
+	end
+	TriggerEvent('es:setMoneyDisplay', 0.0)
+	ESX.UI.HUD.SetDisplay(0.0)
+	
+-- Updates the UI on start
+	NetworkSetTalkerProximity(10.0)
+end)
+
+RegisterNetEvent('esx:playerLoaded')
+AddEventHandler('esx:playerLoaded', function(xPlayer) 
+	local data = xPlayer
+	local accounts = data.accounts
+	for k,v in pairs(accounts) do
+		local account = v
+		if account.name == "bank" then
+			SendNUIMessage({action = "setValue", key = "bankmoney", value = "$"..account.money})
+		elseif account.name == "black_money" then
+			SendNUIMessage({action = "setValue", key = "dirtymoney", value = "$"..account.money})
+-- Society pridani		
+		elseif account.name == "society_money" then
+			SendNUIMessage({action = "setValue", key = "society_money", value = "$"..account.money})
+		end
+	 
+	end
+
+-- Job
+	local job = data.job
+	SendNUIMessage({action = "setValue", key = "job", value = job.label.." - "..job.grade_label, icon = job.name})
+
+-- Money
+	SendNUIMessage({action = "setValue", key = "money", value = "$"..data.money})
+	
+-- Society -chybi?	
+	
+end)
+
+Citizen.CreateThread(function()
+	while true do
+		Citizen.Wait(0)
+		ESX.UI.HUD.SetDisplay(0.0)
+		if isTalking == false then
+			if NetworkIsPlayerTalking(PlayerId()) then
+				isTalking = true
+				SendNUIMessage({action = "setTalking", value = true})
+			end
+		else
+			if NetworkIsPlayerTalking(PlayerId()) == false then
+				isTalking = false
+				SendNUIMessage({action = "setTalking", value = false})
+			end
+		end
+	end
+end)
+
+-- Voice
+
+local prox = 26.0 -- Sets the Default Voice Distance
+local allowProximityChange = true -- Set to True to allow Changing Voice Distance
+
+
+
+RegisterNetEvent('ui:toggle')
+AddEventHandler('ui:toggle', function(show)
+	SendNUIMessage({action = "toggle", show = show})
+end)
+
+RegisterNetEvent('esx:setAccountMoney')
+AddEventHandler('esx:setAccountMoney', function(account)
+	if account.name == "bank" then
+		SendNUIMessage({action = "setValue", key = "bankmoney", value = "$"..account.money})
+	elseif account.name == "black_money" then
+		SendNUIMessage({action = "setValue", key = "dirtymoney", value = "$"..account.money})
+	end
+end)
+
+-- Society
+RegisterNetEvent('esx_addonaccount:setMoney')
+AddEventHandler('esx_addonaccount:setMoney', function(society, money)
+	if ESX.PlayerData.job and ESX.PlayerData.job.grade_name == 'boss' and 'society_' .. ESX.PlayerData.job.name == society then
+		SendNUIMessage({action = "setValue", key = "society_money", value = "$"..account.money})
+	end
+end)
+
+RegisterNetEvent('esx:setJob')
+AddEventHandler('esx:setJob', function(job)
+  SendNUIMessage({action = "setValue", key = "job", value = job.label.." - "..job.grade_label, icon = job.name})
+end)
+
+RegisterNetEvent('es:activateMoney')
+AddEventHandler('es:activateMoney', function(e)
+	SendNUIMessage({action = "setValue", key = "money", value = "$"..e})
+end)
+
+RegisterNetEvent('esx_customui:updateStatus')
+AddEventHandler('esx_customui:updateStatus', function(status)
+	SendNUIMessage({action = "updateStatus", status = status})
+end)
+
+RegisterNetEvent('esx_customui:updateWeight')
+AddEventHandler('esx_customui:updateWeight', function(weight)
+	weightprc = (weight/8000)*100
+	SendNUIMessage({action = "updateWeight", weight = weightprc})
+end)
